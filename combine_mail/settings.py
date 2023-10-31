908224133b2d27a30571f04255c8f875ea87c9c4
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-31 12:41:46 +0600

    use TLS for authentication

--- combine_mail/settings.py
+++ combine_mail/settings.py
@@ -155,6 +155,7 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
